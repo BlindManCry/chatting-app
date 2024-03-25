1288c3007c4866bc386b032987f4a8879fa723c9
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-03-25 18:49:15 +0100

    make chat sidebar

---
+++ src/components/Cat.js
@@ -0,0 +1,12 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+
+const Cat = () => {
+  return (
+    <div>
+      <Avatar src="https://i.pinimg.com/564x/4e/68/a0/4e68a025e2373349bd1dc291a9b0acbc.jpg" />
+    </div>
+  );
+};
+
+export default Cat;
